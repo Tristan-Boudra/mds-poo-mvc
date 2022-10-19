96b29ed12366ee5c8bbbcc003554b4c3cbde9b83
Tristan-Boudra <boudratristan@gmail.com>
2022-10-19 11:31:53 +0200

    ex2

---
+++ poo-mvc-exercise/resources/views/post.blade.php
@@ -0,0 +1,7 @@
+<article>
+    <h1 href="/posts">My first blog post</h1>
+
+    <p>
+        Anim irure mollit qui id veniam et ut dolor Lorem mollit ut. Minim velit est anim esse nulla proident non consectetur officia. In Lorem est amet labore commodo laboris veniam reprehenderit ullamco labore elit. Consectetur reprehenderit culpa minim qui cupidatat irure ut fugiat velit reprehenderit incididunt sit ad anim.
+    </p>
+</article>
---
+++ poo-mvc-exercise/resources/views/posts.blade.php
@@ -0,0 +1,23 @@
+<article>
+    <h1><a href="/post">My first blog post</a></h1>
+
+    <p>
+        Anim irure mollit qui id veniam et ut dolor Lorem mollit ut. Minim velit est anim esse nulla proident non consectetur officia. In Lorem est amet labore commodo laboris veniam reprehenderit ullamco labore elit. Consectetur reprehenderit culpa minim qui cupidatat irure ut fugiat velit reprehenderit incididunt sit ad anim.
+    </p>
+</article>
+
+<article>
+    <h1><a href="/post">My second blog post</a></h1>
+
+    <p>
+        Anim irure mollit qui id veniam et ut dolor Lorem mollit ut. Minim velit est anim esse nulla proident non consectetur officia. In Lorem est amet labore commodo laboris veniam reprehenderit ullamco labore elit. Consectetur reprehenderit culpa minim qui cupidatat irure ut fugiat velit reprehenderit incididunt sit ad anim.
+    </p>
+</article>
+
+<article>
+    <h1><a href="/post">My third blog post</a></h1>
+
+    <p>
+        Anim irure mollit qui id veniam et ut dolor Lorem mollit ut. Minim velit est anim esse nulla proident non consectetur officia. In Lorem est amet labore commodo laboris veniam reprehenderit ullamco labore elit. Consectetur reprehenderit culpa minim qui cupidatat irure ut fugiat velit reprehenderit incididunt sit ad anim.
+    </p>
+</article>
