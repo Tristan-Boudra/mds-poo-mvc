58866d5f5975d016e5428fa3494a3eab4f5b4d85
Tristan-Boudra <boudratristan@gmail.com>
2022-10-19 14:56:03 +0200

    ex4

--- poo-mvc-exercise/resources/views/posts.blade.php
+++ poo-mvc-exercise/resources/views/posts.blade.php
@@ -1,23 +1,45 @@
-<article>
-    <h1><a href="/posts/my-first-post">My first blog post</a></h1>
-
-    <p>
-        Anim irure mollit qui id veniam et ut dolor Lorem mollit ut. Minim velit est anim esse nulla proident non consectetur officia. In Lorem est amet labore commodo laboris veniam reprehenderit ullamco labore elit. Consectetur reprehenderit culpa minim qui cupidatat irure ut fugiat velit reprehenderit incididunt sit ad anim.
-    </p>
-</article>
-
-<article>
-    <h1><a href="/posts/my-second-post">My second blog post</a></h1>
-
-    <p>
-        Anim irure mollit qui id veniam et ut dolor Lorem mollit ut. Minim velit est anim esse nulla proident non consectetur officia. In Lorem est amet labore commodo laboris veniam reprehenderit ullamco labore elit. Consectetur reprehenderit culpa minim qui cupidatat irure ut fugiat velit reprehenderit incididunt sit ad anim.
-    </p>
-</article>
-
-<article>
-    <h1><a href="/posts/my-third-post">My third blog post</a></h1>
-
-    <p>
-        Anim irure mollit qui id veniam et ut dolor Lorem mollit ut. Minim velit est anim esse nulla proident non consectetur officia. In Lorem est amet labore commodo laboris veniam reprehenderit ullamco labore elit. Consectetur reprehenderit culpa minim qui cupidatat irure ut fugiat velit reprehenderit incididunt sit ad anim.
-    </p>
-</article>
+<html lang="fr">
+<head>
+  <meta charset="utf-8">
+  <title>Titre de la page</title>
+  <link rel="stylesheet" href="style.css">
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@300&display=swap" rel="stylesheet">
+  <script src="script.js"></script>
+</head>
+<body>
+    <h1 class="blog">Blog</h1>
+    <div>
+        <li class="liste">
+            <ul>
+                <article>
+                    <h1><a href="/posts/my-first-post">My first blog post</a></h1>
+                
+                    <p>
+                        Anim irure mollit qui id veniam et ut dolor Lorem mollit ut. Minim velit est anim esse nulla proident non consectetur officia. In Lorem est amet labore commodo laboris veniam reprehenderit ullamco labore elit. Consectetur reprehenderit culpa minim qui cupidatat irure ut fugiat velit reprehenderit incididunt sit ad anim.
+                    </p>
+                </article>
+            </ul>
+            <ul>
+                <article>
+                    <h1><a href="/posts/my-second-post">My second blog post</a></h1>
+                
+                    <p>
+                        Anim irure mollit qui id veniam et ut dolor Lorem mollit ut. Minim velit est anim esse nulla proident non consectetur officia. In Lorem est amet labore commodo laboris veniam reprehenderit ullamco labore elit. Consectetur reprehenderit culpa minim qui cupidatat irure ut fugiat velit reprehenderit incididunt sit ad anim.
+                    </p>
+                </article>
+            </ul>
+            <ul>
+                <article>
+                    <h1><a href="/posts/my-third-post">My third blog post</a></h1>
+                
+                    <p>
+                        Anim irure mollit qui id veniam et ut dolor Lorem mollit ut. Minim velit est anim esse nulla proident non consectetur officia. In Lorem est amet labore commodo laboris veniam reprehenderit ullamco labore elit. Consectetur reprehenderit culpa minim qui cupidatat irure ut fugiat velit reprehenderit incididunt sit ad anim.
+                    </p>
+                </article>
+            </ul>
+        </li>
+    </div>
+</body>
+</html>
--- poo-mvc-exercise/routes/web.php
+++ poo-mvc-exercise/routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Models\Post;
+
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -22,12 +24,8 @@ Route::get('/posts', function () {
 });
 
 Route::get('/posts/{post_name}', function ($post_name) {
-    $file_name = __DIR__ . '/../resources/posts/' . $post_name . '.html';
     
-    if (!file_exists($file_name)) {
-        return abort(404);
-    }
-
-    $post_content = file_get_contents($file_name);
-    return view('post', ['post_content' => $post_content]);
+    return view('post', [
+        'post_content' => Post::find($post_name)
+    ]);
 });
